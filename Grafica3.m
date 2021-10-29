efa997159be495b3f74446b641b60b6d9b1417e3
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-28 19:59:11 -0500

    Create Grafica3.m

---
+++ Grafica3.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title         :Grafica 3
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion
+% Date          :28/10/2021
+% Version       :1
+% Usage         :octave
+%               :
+%               :Requiere de octave
+
+clear 
+% Dominio de la funcion
+t = -100:1:100;
+% Rango de la funcion
+ft = (t)./(2-t);
+% Funcion con el plor
+plot(t, ft)
