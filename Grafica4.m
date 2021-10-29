9002720411810201f1ab0f08ec097076bfb493b4
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-28 20:01:41 -0500

    Create Grafica4.m

---
+++ Grafica4.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title         :Grafica 4
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion
+% Date          :28/10/2021
+% Version       :1
+% Usage         :octave
+%               :
+%               :Requiere de octave
+
+clear
+% Dominio de la funcion
+x=-100:10:100;
+% Rango de la funcion
+fx=(x.^2)+(6*(x));
+% Funcion a plotear
+plot(x, fx)
