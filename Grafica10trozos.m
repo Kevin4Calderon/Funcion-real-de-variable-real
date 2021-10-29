5a5def2d76894cb52aceacfb241bb0dff7eb7b42
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-28 20:27:51 -0500

    Create Grafica10trozos.m

---
+++ Grafica10trozos.m
@@ -0,0 +1,18 @@
+% Octave Script
+% Title         :Primera funcion de trozo
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion
+% Date          :28/10/2021
+% Version       :1
+% Usage         :octave
+%               :
+%               :Requiere de octave
+
+clear
+% Dominio de la funcion
+x = -50:10:50;
+fx = (sqrt(x)).*(0<=x<=1).*(2-x).*(1<x<=2)
+% Funcion a plotear
+plot(x, fx)
+grid on;
+title (["(sqrt(x).*(0<=x<=1).*(2-x).*(1<x<=2)"]); 
