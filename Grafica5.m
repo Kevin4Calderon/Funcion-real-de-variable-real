77b857a80463cfbf58823d70b1f9722dd9846242
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-28 20:05:40 -0500

    Create Grafica5.m

---
+++ Grafica5.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title         :Grafica 5
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion
+% Date          :28/10/2021
+% Version       :1
+% Usage         :octave
+%               :
+%               :Requiere de octave
+
+clear
+% Dominio de la función
+z = -100:0.1:100;
+% Rango de la funcion 
+gz = (abs(z).^3);
+% Funcion con el plot 
+plot (z, gz)
